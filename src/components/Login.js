638e55ebe55f76bd2f59a1f5d518ce356adb7c52
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2023-05-31 01:38:57 +0500

    ПР12 В.5 - убрал AuthForm

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,55 +1,53 @@
-import React from "react";
-import { useForm } from "../hooks/useForm";
-import AuthForm from "./AuthForm";
+import React from 'react';
+import { useForm } from '../hooks/useForm';
 
 const Login = ({ onLogin, isLoading }) => {
+
   // Переписал. ПР.12 В.3. Здесь инпуты обрабатываются хуком useForm
   // В Register.js - по-старому, методом и стейтом в самом компоненте.
-  const { values, handleChange, setValues } = useForm({
-    email: "",
-    password: "",
-  });
+  const {values, handleChange, setValues} = useForm({ email: '', password: '' })
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
     onLogin(values);
-  };
+  }
 
-  return (
-    <AuthForm
-      handleSubmit={handleSubmit}
-      formName={"login"}
-      headerText={"Вход"}
-      buttonText={isLoading ? "Вход..." : "Войти"}
-      isLoading={isLoading}
-      formCaption={''}
-    >
-      <input
-        type="email"
-        className="auth__input auth__input_type_email"
-        id="input-email"
-        placeholder="Email"
-        minLength="2"
-        maxLength="30"
-        name="email"
-        value={values.email}
-        onChange={handleChange}
-        required
-      />
-      <span className="auth__error input-email-error"></span>
-      <input
-        type="password"
-        className="auth__input auth__input_type_password"
-        id="input-password"
-        placeholder="Пароль"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-        required
-      />
-      <span className="auth__error input-password-error"></span>
-    </AuthForm>
-  );
-};
+  return(
+    <main className="auth">
+        <h1 className="auth__header">Вход</h1>
+        <form action="#" className="auth__form auth__form_login" name="login" onSubmit={handleSubmit} noValidate>
+          <input
+            type="email"
+            className="auth__input auth__input_type_email"
+            id="input-email"
+            placeholder="Email"
+            minLength="2"
+            maxLength="30"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            required
+          />
+          <span className="auth__error input-email-error"></span>
+          <input
+            type="password"
+            className="auth__input auth__input_type_password"
+            id="input-password"
+            placeholder="Пароль"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            required
+          />
+          <span className="auth__error input-password-error"></span>
+
+          <button className={`auth__button ${isLoading && 'auth__button_disabled'}`} type="submit" disabled={isLoading}>
+            {isLoading ? 'Вход...' : 'Войти'}
+          </button>
+        </form>
+    </main>
+  )
+
+}
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,69 +1,68 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthForm from "./AuthForm";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = ({ onRegister, isLoading }) => {
+
   // Здесь обработки инпутов используется стейт и метод в самом компоненте,
   // А в Login.js переписал на использование хука useState
-  const [formValue, setFormValue] = React.useState({ email: "", password: "" });
+  const [formValue, setFormValue] = React.useState({ email: '', password: '' });
 
   const handleChange = (evt) => {
-    const { name, value } = evt.target;
+    const {name, value} = evt.target;
     setFormValue({
       ...formValue,
-      [name]: value,
+      [name]: value
     });
-  };
+  }
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
     onRegister(formValue);
-  };
+  }
 
-  return (
-    <AuthForm
-      handleSubmit={handleSubmit}
-      formName={"register"}
-      headerText={"Регистрация"}
-      buttonText={isLoading ? "Регистрация..." : "Зарегистрироваться"}
-      isLoading={isLoading}
-      formCaption={
-        <div className="auth__reminder">
-          <p className="auth__caption">
-            Уже зарегистрированы? {" "}
-            <Link to="/sign-up" className="auth__link">
-              Войти
-            </Link>
-          </p>
-        </div>
-      }
-    >
-      <input
-        type="email"
-        className="auth__input auth__input_type_email"
-        id="input-email"
-        placeholder="Email"
-        minLength="2"
-        maxLength="30"
-        name="email"
-        value={formValue.email}
-        onChange={handleChange}
-        required
-      />
-      <span className="auth__error input-email-error"></span>
-      <input
-        type="password"
-        className="auth__input auth__input_type_password"
-        id="input-password"
-        placeholder="Пароль"
-        name="password"
-        value={formValue.password}
-        onChange={handleChange}
-        required
-      />
-      <span className="auth__error input-password-error"></span>
-    </AuthForm>
-  );
-};
+
+  return(
+    <main className="auth">
+        <h1 className="auth__header">Регистрация</h1>
+        <form action="#" className="auth__form auth__form_register" name="register" onSubmit={handleSubmit} noValidate>
+          <input
+            type="email"
+            className="auth__input auth__input_type_email"
+            id="input-email"
+            placeholder="Email"
+            minLength="2"
+            maxLength="30"
+            name="email"
+            value={formValue.email}
+            onChange={handleChange}
+            required
+          />
+          <span className="auth__error input-email-error"></span>
+          <input
+            type="password"
+            className="auth__input auth__input_type_password"
+            id="input-password"
+            placeholder="Пароль"
+            name="password"
+            value={formValue.password}
+            onChange={handleChange}
+            required
+          />
+          <span className="auth__error input-password-error"></span>
+
+          <button className={`auth__button ${isLoading && 'auth__button_disabled'}`} type="submit" disabled={isLoading}>
+            {isLoading ? "Регистрация..." : "Зарегистрироваться"}
+          </button>
+          <div className="auth__reminder">
+            <p className='auth__caption'>Уже зарегистрированы? {" "}
+              <Link to="/sign-up" className="auth__link">Войти</Link>
+            </p>
+          </div>
+        </form>
+    </main>
+  )
+
+}
 
 export default Register;
+
